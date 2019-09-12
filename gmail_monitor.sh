02090d18bf6d68b5cefbbf44cd3c83e40e78ae22
jpward <Ward.j2@Gmail.com>
2019-09-12 11:18:59 -0400

    Updated to run in while loop instead of calling itself (PID overload)

--- gmail_monitor.sh
+++ gmail_monitor.sh
@@ -4,47 +4,50 @@ set -e
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-if ! [ -f /tmp/.meta ]; then
-  cp $HERE/.meta /tmp/.meta
-fi
+work() {
+  if ! [ -f /tmp/.meta ]; then
+    cp $HERE/.meta /tmp/.meta
+  fi
 
-TOK="`cat /tmp/.meta | head -1`"
-RTOK="`cat /tmp/.meta | tail -1`"
-curl -s -H "Authorization: Bearer $TOK" "https://www.googleapis.com/gmail/v1/users/me/threads?q=from:me;label:unread" > /tmp/curl.txt
+  TOK="`cat /tmp/.meta | head -1`"
+  RTOK="`cat /tmp/.meta | tail -1`"
+  curl -s -H "Authorization: Bearer $TOK" "https://www.googleapis.com/gmail/v1/users/me/threads?q=from:me;label:unread" > /tmp/curl.txt
 
-if ! [ -z "`grep '"message": "Invalid Credentials",' /tmp/curl.txt`" ]; then
-  echo woot
-  TMSG="`curl -X POST https://www.googleapis.com/oauth2/v4/token \
-    --header "Content-Type: application/x-www-form-urlencoded" \
-    -d "client_id=<ENTER_CLIENT_ID_HERE!!!>" \
-    -d "client_secret=<ENTER_SECRET_HERE!!!>" \
-    -d "refresh_token=$RTOK" \
-    -d "grant_type=refresh_token"`"
-    TOK="`echo $TMSG | cut -d',' -f1 | sed 's/{ \"access_token\": \"\(.*\)\"/\1/'`"
-    if ! [ -z "$TOK" ]; then
-      echo $TOK > /tmp/.meta
-      echo $RTOK >> /tmp/.meta
-      curl -s -H "Authorization: Bearer $TOK" "https://www.googleapis.com/gmail/v1/users/me/threads?q=from:me;label:unread" > /tmp/curl.txt
-   fi
-fi
+  if ! [ -z "`grep '"message": "Invalid Credentials",' /tmp/curl.txt`" ]; then
+    echo woot
+    TMSG="`curl -X POST https://www.googleapis.com/oauth2/v4/token \
+      --header "Content-Type: application/x-www-form-urlencoded" \
+      -d "client_id=<ENTER_CLIENT_ID_HERE!!!>" \
+      -d "client_secret=<ENTER_SECRET_HERE!!!>" \
+      -d "refresh_token=$RTOK" \
+      -d "grant_type=refresh_token"`"
+      TOK="`echo $TMSG | cut -d',' -f1 | sed 's/{ \"access_token\": \"\(.*\)\"/\1/'`"
+      if ! [ -z "$TOK" ]; then
+        echo $TOK > /tmp/.meta
+        echo $RTOK >> /tmp/.meta
+        curl -s -H "Authorization: Bearer $TOK" "https://www.googleapis.com/gmail/v1/users/me/threads?q=from:me;label:unread" > /tmp/curl.txt
+     fi
+  fi
 
-MSG="`grep -C1 -m1 aut0m8 /tmp/curl.txt || echo ""`"
-if [ -z "$MSG" ]; then
+  MSG="`grep -C1 -m1 aut0m8 /tmp/curl.txt || echo ""`"
+  if [ -z "$MSG" ]; then
+    return
+  fi
+  echo $MSG
+
+  ID="`echo $MSG | cut -d',' -f1 | sed 's/\"id\": \"\(.*\)\"/\1/'`"
+  CMD="`echo $MSG | cut -d',' -f2 | sed 's/\"snippet\": \"\(.*\)\"/\1/'`"
+
+  SCRIPT="`echo $CMD | cut -d':' -f2`"
+  INPUT="`echo $CMD | cut -d':' -f3`"
+
+  curl -H "Authorization: Bearer $TOK" https://www.googleapis.com/gmail/v1/users/me/threads/$ID/trash -X POST --header "Content-length:0" &
+
+  $HERE/${SCRIPT}.sh "$INPUT"
+}
+
+RUN=true
+while $RUN; do
+  work
   sleep 3
-  $0
-fi
-echo $MSG
-
-ID="`echo $MSG | cut -d',' -f1 | sed 's/\"id\": \"\(.*\)\"/\1/'`"
-CMD="`echo $MSG | cut -d',' -f2 | sed 's/\"snippet\": \"\(.*\)\"/\1/'`"
-
-SCRIPT="`echo $CMD | cut -d':' -f2`"
-INPUT="`echo $CMD | cut -d':' -f3`"
-
-curl -H "Authorization: Bearer $TOK" https://www.googleapis.com/gmail/v1/users/me/threads/$ID/trash -X POST --header "Content-length:0" &
-
-$HERE/${SCRIPT}.sh "$INPUT"
-
-sleep 3
-$0
-
+done
