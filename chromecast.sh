e0a0c390d7cce3874740d4af14e66699789fab64
jpward <Ward.j2@Gmail.com>
2020-11-20 09:40:46 -0500

    Update to handle html encodings

--- chromecast.sh
+++ chromecast.sh
@@ -4,5 +4,13 @@ set -e
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+HTML=false
+TXT="$1"
+echo $TXT | grep "&.*;" && HTML=true
+if $HTML; then
+  TXT="$(echo $TXT | python3 -c 'import html, sys; [print(html.unescape(l), end="") for l in sys.stdin]')"
+fi
+
+TXT="$(echo $TXT | sed "s/ ' /'/")"
 #text-2-chromecast
-echo "$1" | nc 192.168.1.121 22223
+echo "$TXT" | nc 192.168.1.121 22223
